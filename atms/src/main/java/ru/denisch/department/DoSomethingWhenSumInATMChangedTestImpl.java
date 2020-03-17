e2bd43cae28ec1ea8c469ec3f355e4d3120428d5
Denis <denisbrd8@gmail.com>
2020-03-17 10:18:38 +0700

    observer

--- atms/src/main/java/ru/denisch/department/DoSomethingWhenSumInATMChangedTestImpl.java
+++ atms/src/main/java/ru/denisch/department/DoSomethingWhenSumInATMChangedTestImpl.java
@@ -5,21 +5,21 @@ import ru.denisch.atm.Atm;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DoSomethingWhenSumInATMChangedTestImpl implements DoSomethingWhenSumInATMChanged{
+public class DoSomethingWhenSumInATMChangedTestImpl implements DoSomethingWhenSumInATMChanged {
 
     // для тестирования
     // количество денег в каждом бакомате
     private static Map<Long, Long> moneyInAtm = new HashMap<>();
 
-    public void doSomething(long sumChange, Atm atm){
+    public static Map<Long, Long> getMoneyInAtm() {
+        return moneyInAtm;
+    }
+
+    public void doSomething(long sumChange, Atm atm) {
         if (moneyInAtm.containsKey(atm.getId())) {
             moneyInAtm.put(atm.getId(), moneyInAtm.get(atm.getId()) + sumChange);
         } else {
             moneyInAtm.put(atm.getId(), sumChange);
         }
-    };
-
-    public static Map<Long, Long> getMoneyInAtm() {
-        return moneyInAtm;
     }
 }
--- atms/src/main/java/ru/denisch/department/Observer.java
+++ atms/src/main/java/ru/denisch/department/Observer.java
@@ -2,8 +2,6 @@ package ru.denisch.department;
 
 import ru.denisch.atm.Atm;
 
-import java.util.Map;
-
 public interface Observer {
     // сумма измнений в банкомате
     void update(long sumChange, Atm atm);
