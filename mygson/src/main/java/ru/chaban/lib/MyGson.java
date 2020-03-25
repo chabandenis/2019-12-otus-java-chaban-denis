8701b9286f6a0cc2251687290108266174bffa38
Denis <denisbrd8@gmail.com>
2020-03-25 10:41:17 +0700

    first

--- mygson/src/main/java/ru/chaban/lib/MyGson.java
+++ mygson/src/main/java/ru/chaban/lib/MyGson.java
@@ -1,125 +1,151 @@
 package ru.chaban.lib;
 
 import javax.json.Json;
-import javax.json.JsonObjectBuilder;
-import java.util.List;
-import java.util.Map;
+import javax.json.JsonArrayBuilder;
 import java.util.Set;
 
 public class MyGson {
 
+    private JsonArrayBuilder jsonArray;
+
     // метод формирования строки JSON
     public String create(Object myObject) throws ClassNotFoundException, IllegalAccessException {
-        return create2(myObject).build().toString();
+        jsonArray = Json.createArrayBuilder();
+        create2(myObject);
+        return jsonArray.build().toString();
     }
 
-    public JsonObjectBuilder create2(Object myObject) throws ClassNotFoundException, IllegalAccessException {
+    public Object create2(Object myObject) throws ClassNotFoundException, IllegalAccessException {
+        //var aB = Json.createArrayBuilder();
         var jO = Json.createObjectBuilder();
 
-        // поля объекта
-        for (var field : myObject.getClass().getDeclaredFields()) {
+        if (myObject.getClass().toString().contains("interface java.util.Set")
+                || myObject.getClass().toString().contains("class java.util.HashSet")
+        ) {
+            var jsonArrayBuilder = Json.createArrayBuilder();
 
-            field.setAccessible(true);
+            for (var item : (Set) myObject) {
+                create2(item);
+            }
+            //aB.add(jsonArrayBuilder);
+        }
+        /*
+        case (): {
+            var jsonArrayBuilder = Json.createArrayBuilder();
 
-            if (field.get(myObject) == null) {
-                continue;
+            for (var item : (Set) field.get(myObject)) {
+                //((Set) field.get(myObject)).size()
+                jsonArrayBuilder.add(item.toString());
             }
 
-            switch (String.valueOf(field.getType())) {
-                case ("interface java.util.Set"): {
-                    var jsonArrayBuilder = Json.createArrayBuilder();
+            jO.add(field.getName(), jsonArrayBuilder);
+            break;
+        }
 
-                    for (var item : (Set) field.get(myObject)) {
-                        //((Set) field.get(myObject)).size()
-                        jsonArrayBuilder.add(item.toString());
-                    }
+        case ("interface java.util.List"): {
+            var jsonArrayBuilder = Json.createArrayBuilder();
 
-                    jO.add(field.getName(), jsonArrayBuilder);
-                    break;
+            for (var item : (List) field.get(myObject)) {
+                jsonArrayBuilder.add(item.toString());
+            }
+
+            jO.add(field.getName(), jsonArrayBuilder);
+
+            break;
+        }
+
+        case ("interface java.util.Map"): {
+            var jsonArrayBuilder = Json.createArrayBuilder();
+            var jsonObjectBuilder = Json.createObjectBuilder();
+
+            Map tmpMap = (Map) field.get(myObject);
+
+            for (var item : tmpMap.keySet()) {
+                jsonObjectBuilder.add(item.toString(), tmpMap.get(item).toString());
+            }
+            //jsonArrayBuilder.add(jsonObjectBuilder);
+            jO.add(field.getName(), jsonObjectBuilder);
+
+            break;
+        }
+
+        case ("interfsace java.util.Map"): {
+            var jsonArrayBuilder = Json.createArrayBuilder();
+            var jsonObjectBuilder = Json.createObjectBuilder();
+
+            Map tmpMap = (Map) field.get(myObject);
+
+            for (var item : tmpMap.keySet()) {
+                jsonObjectBuilder.add(item.toString(), tmpMap.get(item).toString());
+            }
+            jsonArrayBuilder.add(jsonObjectBuilder);
+            jO.add(field.getName(), jsonArrayBuilder);
+
+            break;
+        }
+
+         */
+        else {
+            // поля объекта
+            for (var field : myObject.getClass().getDeclaredFields()) {
+
+                field.setAccessible(true);
+
+                if (field.get(myObject) == null) {
+                    continue;
                 }
 
-                case ("interface java.util.List"): {
-                    var jsonArrayBuilder = Json.createArrayBuilder();
+                switch (String.valueOf(field.getType())) {
 
-                    for (var item : (List) field.get(myObject)) {
-                        jsonArrayBuilder.add(item.toString());
-                    }
+                    case ("int"):
+                        jO.add(field.getName(), field.getInt(myObject));
+                        break;
 
-                    jO.add(field.getName(), jsonArrayBuilder);
+                    case ("class java.lang.String"):
+                        jO.add(field.getName(), String.valueOf(field.get(myObject)));
+                        break;
 
-                    break;
+                    case ("class java.lang.Integer"):
+                        jO.add(field.getName(), Integer.parseInt(field.get(myObject).toString()));
+                        break;
+
+                    case ("class java.lang.Boolean"):
+                        jO.add(field.getName(), (Boolean) field.get(myObject));
+                        break;
+
+                    case ("char"):
+                        jO.add(field.getName(), String.valueOf(field.get(myObject)));
+                        break;
+
+                    case ("class java.lang.Double"):
+                        jO.add(field.getName(), (Double) field.get(myObject));
+                        break;
+
+                    case ("class java.lang.Float"):
+                        jO.add(field.getName(), Double.valueOf(field.get(myObject).toString()));
+                        break;
+
+                    case ("class java.lang.Short"):
+                        jO.add(field.getName(), (Short) field.get(myObject));
+                        break;
+
+                    case ("class java.lang.Long"):
+                        jO.add(field.getName(), (Long) field.get(myObject));
+                        break;
+
+                    case ("long"):
+                        jO.add(field.getName(), (Long) field.get(myObject));
+                        break;
+
+/*                    default:
+                        jO.add(field.getName(), create2(field.get(myObject)));
+
+ */
                 }
-
-                case ("interface java.util.Map"): {
-                    var jsonArrayBuilder = Json.createArrayBuilder();
-                    var jsonObjectBuilder = Json.createObjectBuilder();
-
-                    Map tmpMap = (Map) field.get(myObject);
-
-                    for (var item : tmpMap.keySet()) {
-                        jsonObjectBuilder.add(item.toString(), tmpMap.get(item).toString());
-                    }
-                    //jsonArrayBuilder.add(jsonObjectBuilder);
-                    jO.add(field.getName(), jsonObjectBuilder);
-
-                    break;
-                }
-
-                case ("interfsace java.util.Map"): {
-                    var jsonArrayBuilder = Json.createArrayBuilder();
-                    var jsonObjectBuilder = Json.createObjectBuilder();
-
-                    Map tmpMap = (Map) field.get(myObject);
-
-                    for (var item : tmpMap.keySet()) {
-                        jsonObjectBuilder.add(item.toString(), tmpMap.get(item).toString());
-                    }
-                    jsonArrayBuilder.add(jsonObjectBuilder);
-                    jO.add(field.getName(), jsonArrayBuilder);
-
-                    break;
-                }
-                case ("int"):
-                    jO.add(field.getName(), field.getInt(myObject));
-                    break;
-
-                case ("class java.lang.String"):
-                    jO.add(field.getName(), String.valueOf(field.get(myObject)));
-                    break;
-
-                case ("class java.lang.Integer"):
-                    jO.add(field.getName(), Integer.parseInt(field.get(myObject).toString()));
-                    break;
-
-                case ("class java.lang.Boolean"):
-                    jO.add(field.getName(), (Boolean) field.get(myObject));
-                    break;
-
-                case ("char"):
-                    jO.add(field.getName(), String.valueOf(field.get(myObject)));
-                    break;
-
-                case ("class java.lang.Double"):
-                    jO.add(field.getName(), (Double) field.get(myObject));
-                    break;
-
-                case ("class java.lang.Float"):
-                    jO.add(field.getName(), Double.valueOf(field.get(myObject).toString()));
-                    break;
-
-                case ("class java.lang.Short"):
-                    jO.add(field.getName(), (Short) field.get(myObject));
-                    break;
-
-                case ("class java.lang.Long"):
-                    jO.add(field.getName(), (Long) field.get(myObject));
-                    break;
-
-                default:
-                    jO.add(field.getName(), create2(field.get(myObject)));
+                jsonArray.add(jO);
             }
         }
-        return jO;
+  //      return aB.add(jO);
 
 
     }
