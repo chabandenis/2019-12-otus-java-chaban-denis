5e2ce450c229cfb90268aea331b413a82c21d6d4
Denis <denisbrd8@gmail.com>
2020-04-04 18:48:49 +0700

    first version

---
+++ orm/src/main/java/ru/chaban/jdbc/Demo.java
@@ -0,0 +1,35 @@
+package ru.chaban.jdbc;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Demo {
+    private static final String URL = "jdbc:h2:./test";
+    private static Logger logger = LoggerFactory.getLogger(Demo.class);
+    private final Connection connection;
+
+    public Demo() throws SQLException {
+        this.connection = DriverManager.getConnection(URL, "sa", "");
+        this.connection.setAutoCommit(false);
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Demo demo = new Demo();
+        demo.createTable();
+
+
+    }
+
+    private void createTable() throws SQLException {
+        try (PreparedStatement pst = connection.prepareStatement(
+                "create table test(id int, name varchar2(100))")) {
+            pst.executeUpdate();
+        }
+    }
+}
