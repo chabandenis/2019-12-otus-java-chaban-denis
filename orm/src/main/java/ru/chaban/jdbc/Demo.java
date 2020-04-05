f1c40fdbe6faf0cc9ff87cda2e1d16b38af9a30e
Den <denisbrd8@gmail.com>
2020-04-05 21:25:27 +0700

    2

--- orm/src/main/java/ru/chaban/jdbc/Demo.java
+++ orm/src/main/java/ru/chaban/jdbc/Demo.java
@@ -4,10 +4,7 @@ package ru.chaban.jdbc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Demo {
     private static final String URL = "jdbc:h2:mem:";
@@ -22,14 +19,52 @@ public class Demo {
     public static void main(String[] args) throws SQLException {
         Demo demo = new Demo();
         demo.createTable();
+        demo.insertRecord(1);
+        demo.selectRecord(1);
+        demo.close();
+    }
 
-
+    private void close() throws SQLException {
+       this.connection.close();
     }
 
     private void createTable() throws SQLException {
         try (PreparedStatement pst = connection.prepareStatement(
                 "create table test(id int, name varchar2(100))")) {
             pst.executeUpdate();
+        }
+    }
+
+    private void insertRecord(int id) throws SQLException {
+        try (PreparedStatement preparedStatement =
+                     connection.prepareStatement("insert into test(id, name) values (?, ?)")) {
+            Savepoint savepoint = this.connection.setSavepoint("myPoint");
+            preparedStatement.setInt(1, id);
+            preparedStatement.setString(2, "NameValue");
+
+            try {
+                int rowCount = preparedStatement.executeUpdate();
+                this.connection.commit();
+                logger.info("inserted rowCount: {} ", rowCount);
+            } catch (SQLException ex) {
+                this.connection.rollback(savepoint);
+                logger.error(ex.getMessage(), ex);
+            }
+        }
+    }
+
+    private void selectRecord(int id) throws SQLException {
+        try (PreparedStatement preparedStatement =
+                     this.connection.prepareStatement("select name from test where id = ?")) {
+            preparedStatement.setInt(1, id);
+            try(ResultSet rs = preparedStatement.executeQuery()){
+                StringBuilder ourString = new StringBuilder();
+                ourString.append("name: " );
+                if (rs.next()){
+                    ourString.append(rs.getString("name"));
+                }
+                logger.info(ourString.toString());
+            }
         }
     }
 }
