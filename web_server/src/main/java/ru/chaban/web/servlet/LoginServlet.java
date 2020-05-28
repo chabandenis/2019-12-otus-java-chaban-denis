cc3fa708430785813334a9c5d27534a5ad75c819
Denis <denisbrd8@gmail.com>
2020-05-28 18:29:50 +0700

    1

--- web_server/src/main/java/ru/chaban/web/servlet/LoginServlet.java
+++ web_server/src/main/java/ru/chaban/web/servlet/LoginServlet.java
@@ -1,37 +1,51 @@
 package ru.chaban.web.servlet;
 
-
-
-import ru.chaban.core.dao.UserDao;
 import ru.chaban.web.services.TemplateProcessor;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.Collections;
 
 public class LoginServlet extends HttpServlet {
 
-    private static final String USERS_PAGE_TEMPLATE = "login.html";
-    private static final String TEMPLATE_ATTR_RANDOM_USER = "randomUser";
+    private static final String PARAM_LOGIN = "login";
+    private static final String PARAM_PASSWORD = "password";
+    private static final int MAX_INACTIVE_INTERVAL = 30;
+    private static final String LOGIN_PAGE_TEMPLATE = "login.html";
+
 
     private final TemplateProcessor templateProcessor;
+//    private final UserAuthService userAuthService;
 
     public LoginServlet(TemplateProcessor templateProcessor) {
+        //       this.userAuthService = userAuthService;
         this.templateProcessor = templateProcessor;
-
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws IOException {
-        Map<String, Object> paramsMap = new HashMap<>();
-//        userDao.findRandomUser().ifPresent(randomUser -> paramsMap.put(TEMPLATE_ATTR_RANDOM_USER, randomUser));
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().println(templateProcessor.getPage(USERS_PAGE_TEMPLATE, paramsMap));
+        response.getWriter().println(templateProcessor.getPage(LOGIN_PAGE_TEMPLATE, Collections.emptyMap()));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String name = request.getParameter(PARAM_LOGIN);
+        String password = request.getParameter(PARAM_PASSWORD);
+
+        //if (userAuthService.authenticate(name, password)) {
+        HttpSession session = request.getSession();
+        session.setMaxInactiveInterval(MAX_INACTIVE_INTERVAL);
+        response.sendRedirect("/users");
+      /*  } else {
+            response.setStatus(SC_UNAUTHORIZED);
+        }
+*/
     }
 
 }
